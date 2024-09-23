f229c5bec0ceb827c8d3668746b7fccbd4989651
FaulJacopo <jacopo.faul@samtrevano.ch>
2024-09-23 11:31:41 +0200

    Aggiunta nuova API

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from geopy.geocoders import Nominatim
-import requests, json
+import requests, json, random, string
 from functools import wraps
 from flask import Flask, render_template, request, flash, redirect, url_for, abort
 from flask_login import login_user, login_required, logout_user, current_user, LoginManager
@@ -19,7 +18,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-
 class ProtectedModelView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated
@@ -28,6 +26,35 @@ class ProtectedModelView(ModelView):
         # redirect to login page if user doesn't have access
         return redirect(url_for('auth.login', next=request.url))
 
+meme_list = [
+    "10-Guy", "1950s-Middle-Finger", "1990s-First-World-Problems", "1st-World-Canadian-Problems", "2nd-Term-Obama",
+    "Aaaaand-Its-Gone", "Ace-Primo", "Actual-Advice-Mallard", "Adalia-Rose", "Admiral-Ackbar-Relationship-Expert",
+    "Advice-Dog", "Advice-Doge", "Advice-God", "Advice-Peeta", "Advice-Tam", "Advice-Yoda", "Afraid-To-Ask-Andy",
+    "Afraid-To-Ask-Andy-Closeup", "Aint-Nobody-Got-Time-For-That", "Alan-Greenspan", "Alarm-Clock", "Albert-Cagestein",
+    "Albert-Einstein-1", "Alien-Meeting-Suggestion", "Alright-Gentlemen-We-Need-A-New-Idea", "Always-Has-Been",
+    "Alyssa-Silent-Hill", "Am-I-The-Only-One-Around-Here", "American-Chopper-Argument", "Ancient-Aliens",
+    "And-everybody-loses-their-minds", "And-then-I-said-Obama", "Angry-Asian", "Angry-Baby", "Angry-Birds-Pig",
+    "Angry-Bride", "Angry-Chef-Gordon-Ramsay", "Angry-Chicken-Boss", "Angry-Dumbledore", "Angry-Koala", "Angry-Rant-Randy",
+    "Angry-Toddler", "Annoying-Childhood-Friend", "Annoying-Facebook-Girl", "Anri-Stares", "Anti-Joke-Chicken",
+    "Apathetic-Xbox-Laser", "Archer", "Are-Your-Parents-Brother-And-Sister", "Are-you-a-Wizard", "Arrogant-Rich-Man",
+    "Art-Attack", "Art-Student-Owl", "Arthur-Fist", "Asshole-Ref", "Aunt-Carol", "Austin-Powers-Honestly", "Aw-Yeah-Rage-Face",
+    "Awkward-Moment-Sealion", "Awkward-Olympics", "BANE-AND-BRUCE", "BM-Employees", "Babushkas-On-Facebook", "Baby-Cry",
+    "Baby-Godfather", "Baby-Insanity-Wolf", "Back-In-My-Day", "Bad-Advice-Cat", "Bad-Joke-Eel", "Bad-Luck-Bear",
+    "Bad-Luck-Brian", "Bad-Luck-Hannah", "Bad-Pun-Anna-Kendrick", "Bad-Pun-Dog", "Bad-Wife-Worse-Mom", "Bah-Humbug", "Bane",
+    "Bane-Permission", "Barack-And-Kumar-2013", "Barba", "Barbosa-And-Sparrow", "Barney-Stinson-Win", "Baromney", "Baron-Creater",
+    "Bart-Simpson-Peeking", "Batman-And-Superman", "Batman-Slapping-Robin", "Batman-Smiles", "Batmobile", "Bazooka-Squirrel",
+    "Be-Like-Bill", "Bear-Grylls", "Beard-Baby", "Bebo", "Because-Race-Car", "Ben-Barba-Pointing", "Bender", "Benito",
+    "Bernie-I-Am-Once-Again-Asking-For-Your-Support", "Beyonce-Knowles-Superbowl", "Beyonce-Knowles-Superbowl-Face",
+    "Beyonce-Superbowl-Yell", "Big-Bird", "Big-Bird-And-Mitt-Romney", "Big-Bird-And-Snuffy", "Big-Ego-Man", "Big-Family-Comeback",
+    "Bike-Fall", "Bill-Murray-Golf", "Bill-Nye-The-Science-Guy", "Bill-OReilly", "Billy-Graham-Mitt-Romney", "Bitch-Please",
+    "Black-Girl-Wat", "Blank-Blue-Background", "Blank-Colored-Background", "Blank-Comic-Panel-1x2", "Blank-Comic-Panel-2x1",
+    "Blank-Comic-Panel-2x2", "Blank-Nut-Button", "Blank-Starter-Pack", "Blank-Transparent-Square", "Blank-Yellow-Sign", "Blob",
+    "Blue-Futurama-Fry", "Boardroom-Meeting-Suggestion", "Bonobo-Lyfe", "Booty-Warrior", "Bothered-Bond", "Brace-Yourselves-X-is-Coming",
+    "Brian-Burke-On-The-Phone", "Brian-Griffin", "Brian-Williams-Was-There", "Brian-Williams-Was-There-2", "Brian-Williams-Was-There-3",
+    "Brian-Wilson-Vs-ZZ-Top", "Britney-Spears", "Bubba-And-Barack", "Buddy-Christ", "Buddy-The-Elf", "Buff-Doge-vs-Cheems", "Bullets",
+    "Burn-Kitty", "Business-Cat", "But-Thats-None-Of-My-Business", "But-Thats-None-Of-My-Business-Neutral", "Butthurt-Dweller",
+    "CASHWAG-Crew", "CURLEY", "Captain-Hindsight", "Captain-Phillips-Im-The-Captain-Now", "Captain-Picard-Facepalm", "Car-Salesman-Slaps-Hood"
+]
 
 admin = Admin(app, name='Admin dashboard', template_mode='bootstrap4')
 admin.add_view(ProtectedModelView(User, db.session))
@@ -94,19 +121,15 @@ def weather_single():
     city = values["city"]
     return weather(city)
 
-@app.route('/weather/<city>')
-def weather(city):
-    add_request_to_DB(city)
+@app.route('/meme/<text1>&<text2>')
+def getMeme(text1, text2):
+    random_meme = random.sample(meme_list, 1)
+    text1 = str(text1).replace(" ", "+")
+    text2 = str(text2).replace(" ", "+")
+    random_meme = (str(random_meme).replace("'", "")).replace('[', "")
+    api_request = f"http://apimeme.com/meme?meme={random_meme.replace(']',"")}&top={text1}&bottom={text2}"
+    return f'<img src="{api_request}" />'
 
-    geolocator = Nominatim(user_agent="MyApp")
-    location = geolocator.geocode(city)
-    latitude = location.latitude
-    longitude = location.longitude
-
-    headers = {'Content-type': 'Application/json'}
-    requestPost = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
-    res = requests.post(requestPost, headers=headers)
-    return f"{res.json()}"
 
 @app.route('/hello/<username>')
 def hello(username):
